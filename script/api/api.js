f963837f4212e2b04028fc3f183e3c59ea8a5306
alkubaisiothman <othman_hotman@hotmail.com>
2025-04-22 23:00:07 +0300

    päivitetty api.js

--- script/api/api.js
+++ script/api/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://tradetracker.onrender.com';
 
 const handleResponse = async (response) => {
   if (!response.ok) {
