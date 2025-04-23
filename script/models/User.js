4547469c77e3aa12c94f2a5cffa2a13aac58c474
alkubaisiothman <othman_hotman@hotmail.com>
2025-04-23 16:15:54 +0300

    hashaus salasanoille

--- script/models/User.js
+++ script/models/User.js
@@ -1,44 +1,23 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength: 3,
-    maxlength: 20,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    select: false
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, minlength: 3, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6, select: false },
+  createdAt: { type: Date, default: Date.now }
 });
 
-// Salasanan hashays
-UserSchema.pre('save', async function (next) {
+// 🔐 Hashaa salasana ennen tallennusta
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Vertailu funktio
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+// 🔐 Salasanan vertailu kirjautuessa
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
