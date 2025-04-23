da25b0502a5b68f6fb6c291ac1329e6c19e8f40a
alkubaisiothman <othman_hotman@hotmail.com>
2025-04-23 03:35:13 +0300

    testi2

--- server.js
+++ server.js
@@ -55,12 +55,6 @@ const apiLimiter = rateLimit({
 // Sovellusmiddlewaret
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(cors({
-  origin: 'https://trade-track.netlify.app',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 // Staattisten tiedostojen tarjoaminen
 app.use(express.static(path.join(__dirname, 'public')));
